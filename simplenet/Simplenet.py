fix loss formula

simplenet/Simplenet.py
@@ -59,5 +59,5 @@ class Simplenet(torch.nn.Module):
     
     @staticmethod
     def loss(z_scores_correct, z_scores_altered):
-        return (F.relu(0.5 - z_scores_correct) + F.relu(-0.5 + z_scores_altered)).sum()
+        return F.relu(0.5 - z_scores_correct).sum() + F.relu(-0.5 + z_scores_altered).sum()
 